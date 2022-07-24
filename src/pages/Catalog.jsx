d2d3da4dd495d71d84a293c0b8b48a9487b2d74d
SatrioBayu <sbayu875@gmail.com>
2022-07-24 13:31:58 +0700

    feat: edit css

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -131,7 +131,7 @@ export default function Catalog() {
                   ))}
                 </div>
               ) : (
-                <h3 className="catalog-title text-center">No Product Found</h3>
+                <h3 className="catalog-title mt-5 text-center">No Product Found</h3>
               )}
             </div>
           </div>
